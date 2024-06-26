d1113acc1b19c16ac6a2e4a94a759bd6ed5a5c67
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-06-26 23:11:44 +0000

    Then

--- 0x01-ES6_promise/test/2-then.test.js
+++ 0x01-ES6_promise/test/2-then.test.js
@@ -1,16 +1,15 @@
 import handleResponseFromAPI from '../2-then';
-import getFullResponseFromAPI from '../1-promise';
 
 describe('handleResponseFromAPI', () => {
-  it('should return the correct response for a resolved promise', async () => {
-    const mockPromise = Promise.resolve({ status: 200, body: 'success' });
+  it('should return { status: 200, body: "success" } for a resolved promise', async () => {
+    const mockPromise = Promise.resolve();
 
     const result = await handleResponseFromAPI(mockPromise);
     expect(result).toEqual({ status: 200, body: 'success' });
   });
 
-  it('should return an error object for a rejected promise', async () => {
-    const mockPromise = Promise.reject(new Error('The fake API is not working currently'));
+  it('should return an Error object for a rejected promise', async () => {
+    const mockPromise = Promise.reject();
 
     const result = await handleResponseFromAPI(mockPromise);
     expect(result).toBeInstanceOf(Error);
@@ -18,7 +17,7 @@ describe('handleResponseFromAPI', () => {
 
   it('should log "Got a response from the API" to the console', async () => {
     const consoleSpy = jest.spyOn(console, 'log');
-    const mockPromise = Promise.resolve({ status: 200, body: 'success' });
+    const mockPromise = Promise.resolve();
 
     await handleResponseFromAPI(mockPromise);
     expect(consoleSpy).toHaveBeenCalledWith('Got a response from the API');
@@ -26,4 +25,3 @@ describe('handleResponseFromAPI', () => {
     consoleSpy.mockRestore();
   });
 });
-
