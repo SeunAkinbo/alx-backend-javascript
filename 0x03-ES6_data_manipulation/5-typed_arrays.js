c3d7c5b5733afa85029ededca4f32963d828b771
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-07-02 02:58:55 +0000

    Typed Array && Jest Test

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,13 +1,9 @@
-// createInt8TypedArray.js
-function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(length);
-  const int8Array = new Int8Array(buffer);
+// 5-typed_arrays.js
+export default function createInt8TypedArray(length, position, value) {
   if (position < 0 || position >= length) {
     throw new Error('Position outside range');
   }
-
-  int8Array[position] = value;
-  return buffer;
+  const buf = new DataView(new ArrayBuffer(length), 0, length);
+  buf.setInt8(position, value);
+  return buf;
 }
-
-module.exports = createInt8TypedArray;
--- 0x03-ES6_data_manipulation/tests/5-typed_arrays.test.js
+++ 0x03-ES6_data_manipulation/tests/5-typed_arrays.test.js
@@ -1,15 +1,14 @@
-// createInt8TypedArray.test.js
-const createInt8TypedArray = require('../5-typed_arrays');
+// 5-typed_arrays.test.js
+import createInt8TypedArray from '../5-typed_arrays';
 
-test('createInt8TypedArray returns a new ArrayBuffer with an Int8 value at the specified position', () => {
+test('createInt8TypedArray returns a DataView with an Int8 value at the specified position', () => {
   const length = 10;
   const position = 2;
   const value = 5;
   
-  const buffer = createInt8TypedArray(length, position, value);
-  const int8Array = new Int8Array(buffer);
+  const dataView = createInt8TypedArray(length, position, value);
   
-  expect(int8Array[position]).toBe(value);
+  expect(dataView.getInt8(position)).toBe(value);
 });
 
 test('createInt8TypedArray throws an error if the position is outside the range', () => {
@@ -26,4 +25,14 @@ test('createInt8TypedArray throws an error if the position is negative', () => {
   const value = 5;
 
   expect(() => createInt8TypedArray(length, invalidPosition, value)).toThrow('Position outside range');
+});
+
+test('createInt8TypedArray returns a DataView with correct length', () => {
+  const length = 10;
+  const position = 2;
+  const value = 5;
+  
+  const dataView = createInt8TypedArray(length, position, value);
+  
+  expect(dataView.byteLength).toBe(length);
 });
