53b7d04218126b3606f86a0926767868cc883a86
Oluwaseun Akinbo <seunakinbo@gmail.com>
2024-07-02 16:58:10 +0000

    Update Unique Items && Jest Test

--- 0x03-ES6_data_manipulation/tests/10-update_uniq_items.test.js
+++ 0x03-ES6_data_manipulation/tests/10-update_uniq_items.test.js
@@ -1,22 +1,35 @@
 // 10-update_uniq_items.test.js
 import updateUniqueItems from '../10-update_uniq_items';
-import groceriesList from '../9-groceries_list';
 
 test('updateUniqueItems updates the quantity of items with initial quantity of 1 to 100', () => {
-  const groceries = groceriesList();
-  const updatedGroceries = updateUniqueItems(groceries);
-  
-  expect(updatedGroceries.get('Pasta')).toBe(100);
-  expect(updatedGroceries.get('Rice')).toBe(100);
+  const groceries = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+
+  updateUniqueItems(groceries);
+
+  expect(groceries.get('Pasta')).toBe(100);
+  expect(groceries.get('Rice')).toBe(100);
 });
 
 test('updateUniqueItems does not change the quantity of items with initial quantity other than 1', () => {
-  const groceries = groceriesList();
-  const updatedGroceries = updateUniqueItems(groceries);
-  
-  expect(updatedGroceries.get('Apples')).toBe(10);
-  expect(updatedGroceries.get('Tomatoes')).toBe(10);
-  expect(updatedGroceries.get('Banana')).toBe(5);
+  const groceries = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+
+  updateUniqueItems(groceries);
+
+  expect(groceries.get('Apples')).toBe(10);
+  expect(groceries.get('Tomatoes')).toBe(10);
+  expect(groceries.get('Banana')).toBe(5);
 });
 
 test('updateUniqueItems throws an error if the argument is not a map', () => {
